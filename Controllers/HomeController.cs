209edbc7446673c345860beca086bd02f0bc3b6b
Alex Medvedev <localisseat@gmail.com>
2019-05-12 23:54:28 +0700

    Controller
    HomeController, two ActionResults Index and SearchDriver

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDictionary.Models;
+
+namespace WebDictionary.Controllers
+{
+    public class HomeController : Controller
+    {
+        DriverContext driverContext = new DriverContext();
+        public ActionResult Index()
+        {
+            IEnumerable<Driver> drivers = driverContext.Drivers.ToList();
+
+            return View(drivers);
+        }
+
+        [HttpGet]
+        public ActionResult SearchDriver(string Name)
+        {
+            IEnumerable<Driver> drivers = driverContext.Drivers.ToList();
+
+            return View(drivers.Where(str => str.LastName.Contains(Name)));
+        }
+    }
+}
